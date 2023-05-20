7e4705278cdda02d08369f3bf0e47610ddefad2b
KeremGonen <58744715+KeremGonenn@users.noreply.github.com>
2023-05-20 18:06:14 +0300

    enemy attack

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,19 +8,49 @@ public class Enemy : MonoBehaviour
     private NavMeshAgent agent; // Navigation Mesh Agent bileþeni
 
     public bool isTargetable;
-
+    private Animator animator;
     private Health _health;
+
+    public float saldiriMesafesi = 2f; // Saldýrý mesafesi
+    public float saldiriZamani = 1f; // Saldýrý zaman aralýðý
+    public float saldiriGucu = 10f; // Saldýrý gücü
+    private float sonSaldiriZamani; // Son saldýrý zamaný
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         _health = GetComponent<Health>();
         hedefNokta = EnemyController.Instance.enemyTargetPoint;
+        sonSaldiriZamani = Time.time; // Baþlangýçta son saldýrý zamanýný ayarla
     }
 
     private void Update()
     {
         agent.SetDestination(hedefNokta.position); // Hedef noktaya doðru ilerleme baþlatýlýyor
+        if (agent.remainingDistance <= saldiriMesafesi && Time.time >= sonSaldiriZamani + saldiriZamani)
+        {
+            Saldýr();
+            sonSaldiriZamani = Time.time; // Saldýrý yapýldýðýnda son saldýrý zamanýný güncelle
+        }
+
+    }
+
+    private void Saldýr()
+    {
+        // Hedefe hasar verme iþlemleri burada gerçekleþtirilebilir
+        // Örneðin, hedefin bir Health bileþeni varsa, onun ReduceHealth() metodu kullanýlabilir
+        Health hedefHealth = hedefNokta.GetComponent<Health>();
+        if (hedefHealth != null)
+        {
+            //hedefHealth.ReduceHealth(saldiriGucu);
+            Debug.Log("Hedefe saldýrý yapýldý! Hasar: " + saldiriGucu);
+        }
+        else
+        {
+            animator.SetTrigger("Attack");
+            Debug.Log("Hedefe Hasar: " + saldiriGucu);
+        }
     }
 
     public float GetHealth()
