c81f695254ad7fcf04c1bd3386e01590c76a4a6c
KeremGonen <58744715+KeremGonenn@users.noreply.github.com>
2023-05-21 21:07:30 +0300

    Prefablar ve Spawnpointler

--- Assets/Scripts/Allys.cs
+++ Assets/Scripts/Allys.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class Allys : MonoBehaviour
 {
-    public GameObject askerPrefab; // Asker prefabini atamak için Inspector'dan sürükleyip býrakýn
-    public Transform spawnNoktasi; // Spawn noktasýný atamak için Inspector'dan sürükleyip býrakýn
+    public List<GameObject> soldierPrefabs; // Soldier prefablarýný atamak için Inspector'dan sürükleyip býrakýn
+    public List<Transform> spawnNoktalari; // Spawn noktalarýný atamak için Inspector'dan sürükleyip býrakýn
     public int maksimumSpawnSayisi = 3; // Maksimum spawn sayýsýný ayarlayýn
 
     private int askerSayaci = 0; // Asker sayacý
 
     public void OnSpawnButtonPressed()
     {
-        askerSayaci++; // Sayaçý arttýr
-
-        for (int i = 0; i < Mathf.Min(askerSayaci, maksimumSpawnSayisi); i++)
+        if (askerSayaci >= maksimumSpawnSayisi)
         {
-            GameObject newAsker = Instantiate(askerPrefab, spawnNoktasi.position, spawnNoktasi.rotation);
-
-            // Yeni asker nesnesi ile ilgili istediðiniz ayarlamalarý yapabilirsiniz
-            // Örneðin, askerin hedefini veya hareketini ayarlayabilirsiniz
+            return; // Maksimum spawn sayýsýna ulaþýldýðýnda spawn iþlemini durdur
         }
+
+        int spawnIndex = askerSayaci % spawnNoktalari.Count; // Spawn noktasý indeksini döngü içinde deðiþtir
+        int prefabIndex = askerSayaci % soldierPrefabs.Count; // Soldier prefabý indeksini döngü içinde deðiþtir
+
+        GameObject newAsker = Instantiate(soldierPrefabs[prefabIndex], spawnNoktalari[spawnIndex].position, spawnNoktalari[spawnIndex].rotation);
+
+        // Yeni asker nesnesi ile ilgili istediðiniz ayarlamalarý yapabilirsiniz
+        // Örneðin, askerin hedefini veya hareketini ayarlayabilirsiniz
+
+        askerSayaci++; // Sadece baþarýlý spawn iþlemi sonrasýnda asker sayacýný arttýr
     }
 }
